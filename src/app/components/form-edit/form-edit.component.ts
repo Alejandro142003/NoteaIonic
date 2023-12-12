22eeea614f142c4cf3c0be82dc88460cfe4ade68
Alejandro <gutierrezcrucesalejandro@gmail.com>
2023-12-12 12:53:26 +0100

    Eliminación de notas hecho y avance en edición con modal

---
+++ src/app/components/form-edit/form-edit.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Note } from 'src/app/model/note';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form-edit',
+  standalone: true,
+  templateUrl: './form-edit.component.html',
+  styleUrls: ['./form-edit.component.scss'],
+  imports: [IonicModule, CommonModule, FormsModule],
+})
+export class FormEditComponent {
+  @Input() note!: Note;
+  constructor(private modalCtrl: ModalController) {}
+
+  cancel() {
+    return this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  confirm(note: Note) {
+    return this.modalCtrl.dismiss(note.title,note.description, 'confirm');
+  }
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,20 +3,22 @@ import { IonicModule } from '@ionic/angular';
 import { NoteService } from '../services/note.service';
 import { Note } from '../model/note';
 import { CommonModule } from '@angular/common';
+import { ModalController } from '@ionic/angular';
+import { FormEditComponent } from '../components/form-edit/form-edit.component';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule,CommonModule]
+  imports: [IonicModule, CommonModule],
 })
 export class Tab2Page {
   //public misnotas:Note[]=[];
-  public noteS = inject(NoteService);  //noteS.notes$
-  constructor() {}
+  public noteS = inject(NoteService); //noteS.notes$
+  constructor(private modalCtrl: ModalController) {}
 
-  ionViewDidEnter(){
+  ionViewDidEnter() {
     /*this.misnotas=[];
     this.noteS.readAll().subscribe(d=>{
       console.log(d)
@@ -26,11 +28,20 @@ export class Tab2Page {
     })*/
   }
 
-  editNote(){
+  async editNote(note: Note) {
+    const modal = await this.modalCtrl.create({
+      component: FormEditComponent,
+    });
+    modal.present();
 
+    const { data, role } = await modal.onWillDismiss();
+
+    if (role === 'confirm') {
+      this.noteS.updateNote(note);
+    }
   }
-  deleteNote(){
-
+  deleteNote(note: Note) {
+    this.noteS.deleteNote(note);
+    //Toast implementation
   }
-
 }
