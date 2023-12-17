eae92f32350a1bfa9943b3a5105c7881b4d90192
Alejandro <gutierrezcrucesalejandro@gmail.com>
2023-12-17 19:55:31 +0100

    Unsplash y logo hechos

---
+++ src/app/splash/splash.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.page.html',
+  styleUrls: ['./splash.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class SplashPage {
+
+  constructor(public router:Router) { 
+    setTimeout(()=>{
+      this.router.navigateByUrl('/tabs/tab1');
+  },1800)}
+}
